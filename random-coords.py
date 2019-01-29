f297b9991c10c599498abe2c1cd3b2afdc0171db
cyna-19 <cynamonski@wc-dhcp64d121.student-secure.wireless.fsu.edu>
2019-01-29 14:08:06 -0500

    Python 2 program now prints letter / number coordinate combinations.

--- random-coords.py
+++ random-coords.py
@@ -10,7 +10,7 @@
 
 import random
 import os
-
+  
 def samplegrid(rows, columns, samples):
         try:
                 return [divmod(i, columns) for i in random.sample(range(rows * columns), samples)]
@@ -28,15 +28,18 @@ def samplegrid(rows, columns, samples):
 def triplicate(samplegrid_output):
      var = []
      num = 0
+     alphabet = { 0 : 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10 : 'K', 
+     11: 'L', 12 : 'M', 13 : 'N', 14 : 'O', 15 : 'P', 16 : 'Q', 17 : 'R', 18 : 'S', 19 : 'T', 
+     20 : 'U', 21 : 'V', 22 : 'W', 23 : 'X', 24 : 'Y', 25 : 'Z' }
      for i in range(len(samplegrid_output)+1):
              if i == 0:
                      continue
              if i%3 == 0:
                 num += 1
-                if num % 2 == 0:
-                        print bcolors.WARNING + 'Coordinate #', num, ' : ', samplegrid_output[(i-3):i], bcolors.ENDC
+                if num % 2 != 0:
+                        print bcolors.WARNING + 'Coordinate #', num, ' : ', '(', alphabet[(samplegrid_output[i-3][0])], ',', samplegrid_output[i-3][1], ')', '(', alphabet[(samplegrid_output[i-2][0])], ',', samplegrid_output[i-2][1], ')', '(', alphabet[(samplegrid_output[i-1][0])], ',', samplegrid_output[i-1][1], ')' + bcolors.ENDC
                 else:
-                        print'Coordinate #', num, ' : ', samplegrid_output[(i-3):i]
+                        print'Coordinate #', num, ' : ', '(', alphabet[(samplegrid_output[i-3][0])], ',', samplegrid_output[i-3][1], ')', '(', alphabet[(samplegrid_output[i-2][0])], ',', samplegrid_output[i-2][1], ')', '(', alphabet[(samplegrid_output[i-1][0])], ',', samplegrid_output[i-1][1], ')'
 
 if os.system('defaults read -g AppleInterfaceStyle') == 0:
         class bcolors:
